126305a2c5d585905b06f1b6511f5bec485ae561
Jean Carlos Jimenez <jea_rod5@jeans-mbp.mynetworksettings.com>
2022-05-31 12:39:22 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,47 +1,70 @@
 import React, { Component } from 'react';
 
-import { CardList } from './Components/card-list/card-list.component'
-import { SearchBox } from './Components/search-box/search-box.component';
+//react bootstrap components
+import { CardDeck, Row, Card, Container, Col } from "react-bootstrap/";
 
 import './App.css';
 
+
 class App extends Component {
-  constructor() {
-    super();
+
+   constructor(props) {
+    super(props);
 
     this.state = {
-      monsters: [],
-      searchField: ''
+        items: []
     };
-
-  }
-
-  componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(response => response.json())
-      .then(users => this.setState({ monsters: users }));
-  }
-  handleChange = (e) => {
-    this.setState({ searchField: e.target.value });
-  }
-
-
-  render() {
-    const { monsters, searchField } = this.state;
-    const filteredMonsters = monsters.filter(monster =>
-      monster.name.toLowerCase().includes(searchField.toLowerCase())
-    );
-      return (
-      <div className="App">
-      <h1>MONSTERS ROLODEX</h1>
-        <SearchBox 
-          placeholder='search monsters' 
-          handleChange={this.handleChange}/>
-        <CardList monsters={filteredMonsters}/>
-      </div>
-      );
-    }
 }
 
+componentDidMount() {
+    fetch(
+"https://systems-api.herokuapp.com/systems_list")
+        .then((response) => response.json())
+        //.then(res => console.log(res))
+        .then((response) => {
+            this.setState({
+                items: response
+            });
+        })
+}
+
+render() {
+		return (
+				<Container fluid>
+					<Row>
+            <Col>
+              <CardDeck class="row row-cols-4">
+                {this.state.items.map((postitems) => {
+                  //console.log(postitems);
+                  return (
+                    <Card key={postitems.id}>
+                      <Card.Body grid-gap="20px">
+                      <Card.Img src={`https://robohash.org/${postitems.id}?set=set2&size=180x180`}/>
+                        <Card.Title>
+                          {postitems.system_name}
+                        </Card.Title>
+                        <Card.Subtitle>
+                          {postitems.quality_status}
+                        </Card.Subtitle>
+                        <Card.Text>
+                          {'Capabilities: ' + postitems.capabilities}
+                        </Card.Text>
+                        <Card.Text>
+                          {'Average Availability Speed: ' + postitems.avg_availablity_speed}
+                        </Card.Text>
+                        <Card.Text>
+                          {'Booking Failure Rate: ' + postitems.booking_failure_rate}
+                        </Card.Text>
+                      </Card.Body>
+                    </Card>
+                  );
+                })}
+              </CardDeck>
+            </Col>
+					</Row>
+				</Container>
+		);
+}
+}
 
 export default App;
